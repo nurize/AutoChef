3cf65cccb3e83bd66fc8894e643445925d23933d
kwasi <affulnana04@gmail.com>
2024-07-08 13:51:08 +0000

    project initiated

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from "./pages/Home";
+// import About from "./pages/About";
+// import Contact from "./pages/Contact";
+import GalleryPage from './pages/GalleryPage';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Services from './pages/ServicesPage';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header/>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          {/* <Route path="/services" element={<Services />} />
+          <Route path="/gallery" element={<GalleryPage />} /> */}
+        </Routes>
+        <Footer/>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
