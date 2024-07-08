6f8e2b09ba86a97b5a495cdc3be9d937ee17912f
kwasi <affulnana04@gmail.com>
2024-07-08 16:16:11 +0000

    [Bug-1] Change the directory of the assets folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridAutoFlow: {
+        dense: 'dense',
+      },
+    }
+  },
+  variants: {
+    extend: {
+      scale: ['hover'],
+    },
   },
   plugins: [],
-}
+};
