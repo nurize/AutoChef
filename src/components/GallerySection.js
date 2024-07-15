4f55415bb4c1b90c8c86e067f946a45ec2ff6a45
kwasi <affulnana04@gmail.com>
2024-07-15 12:01:45 +0000

    FR- add gallery page

--- src/components/GallerySection.js
+++ src/components/GallerySection.js
@@ -1,17 +1,17 @@
 import { Link } from "react-router-dom";
-import GalleryComponent from "./GalleryComponent";
+import FeaturedGallery from "./FeaturedGallery";
 
 
 
 const GallerySection = () => {
   return (
-    <div className="container mx-auto px-8 py-8">
-      <h1 className="text-3xl font-bold mb-4">Gallery</h1>
-      <p className="mb-8 text-gray-700">
+    <div className="container py-8 mx-auto ">
+      <h1 className="text-3xl font-bold mb-4 mx-auto px-8">Gallery</h1>
+      <p className="mb-8 text-gray-700 mx-auto px-8">
         Explore our latest automotive triumphs - showcasing precision in every service and genuine parts, ensuring your vehicle excels on every journey.
       </p>
-      <GalleryComponent/>
-      <div className="mt-8 text-right">
+      <FeaturedGallery/>
+      <div className="text-right mx-auto px-8">
         <Link to="/gallery" className="text-red-600 hover:underline">View More</Link>
       </div>
     </div>
--- src/pages/GalleryPage.js
+++ src/pages/GalleryPage.js
@@ -1,23 +1,33 @@
-import GalleryComponent from "../components/GalleryComponent";
+import React from 'react';
 import HeroSection from "../components/HeroSection";
 import heroSections from "../data/heroSections";
+import Gallery from '../components/Gallery';
+import FeaturedGallery from '../components/FeaturedGallery';
 
 const GalleryPage = () => {
   const heroData = heroSections.find(section => section.page === 'gallery');
 
-  return ( 
-    <div className="bg-black">
-      {/* <h2 className="text-red-800 text-center text-5xl font-bold py-10 bg-stone-700 ">Gallery</h2> */}
-
-      {/* <HeroSection 
+  return (
+    <div className="bg-gray-950 min-h-screen">
+      <HeroSection 
         backgroundImage={heroData.backgroundImage}
         title={heroData.title}
         description={heroData.description}
         description2={heroData.description2}
-      /> */}
-      <GalleryComponent/>
+      />
+      <div className="max-w-6xl mx-auto py-16 px-4">
+        <h2 className="text-3xl text-white text-center font-bold mb-8">Our Stunning Gallery</h2>
+        <p className="text-lg text-gray-300 text-center">
+          Explore our collection of stunning images showcasing our exceptional services and the remarkable transformations we bring to your vehicles. Each picture tells a story of dedication, precision, and unmatched quality.
+        </p>
+      </div>
+      <FeaturedGallery/>
+      <div className="max-w-6xl mx-auto py-16 px-4">
+        <h2 className="text-3xl text-white text-center font-bold mb-8">More from Our Gallery</h2>
+      </div>
+        <Gallery />
     </div>
   );
 }
- 
+
 export default GalleryPage;
