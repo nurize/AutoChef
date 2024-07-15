d2983db544a5580d73905593e86d2fac02b509ca
kwasi <affulnana04@gmail.com>
2024-07-15 11:55:13 +0000

    update: add name fields to sign up form

--- src/components/LoginSignupModal.js
+++ src/components/LoginSignupModal.js
@@ -1,22 +1,77 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { FaEnvelope } from 'react-icons/fa';
 import { FcGoogle } from 'react-icons/fc';
-import { FaApple } from "react-icons/fa";
-import { MdOutlineVpnKey } from "react-icons/md";
+import { FaApple } from 'react-icons/fa';
+import { MdOutlineVpnKey } from 'react-icons/md';
+import { FaUser } from "react-icons/fa6";
 
 const LoginSignupModal = ({ isOpen, onClose }) => {
   const [action, setAction] = useState('Sign In');
   const [email, setEmail] = useState('');
+  const [firstname, setFirstname] = useState('');
+  const [lastname, setLastname] = useState('');
   const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === 'Escape') {
+        onClose();
+      }
+    };
+
+    const handleClickOutside = (event) => {
+      if (event.target.classList.contains('modal-overlay')) {
+        onClose();
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener('keydown', handleKeyDown);
+      document.addEventListener('click', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [isOpen, onClose]);
 
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-black z-20 bg-opacity-50 flex justify-center items-center p-4 sm:p-6 md:p-8">
-      <div className="bg-white py-8 px-6 sm:px-10 md:px-14 lg:px-20 rounded-lg shadow-lg w-full sm:w-4/5 md:w-3/5 lg:w-2/5 xl:w-1/3">
+    <div className="fixed inset-0 bg-black z-50 bg-opacity-50 flex justify-center items-center p-4 sm:p-6 md:p-8 modal-overlay">
+      <div className="relative bg-white py-8 px-6 sm:px-10 md:px-14 lg:px-20 rounded-lg shadow-lg w-full sm:w-4/5 md:w-3/5 lg:w-2/5 2xl:w-1/3">
         <h2 className="text-2xl font-bold text-black mb-4 text-center">{action}</h2>
         <p className="text-center text-gray-500 mb-8">Please enter your details</p>
         <form>
+          {action === 'Sign Up' && (
+            <>
+              <div className="mb-4">
+                <label className="block text-gray-700">First Name</label>
+                <div className="relative">
+                  <FaUser className="absolute left-3 top-4 text-gray-400" />
+                  <input
+                    type="text"
+                    className="w-full p-3 pl-10 border border-gray-300 rounded-lg text-black"
+                    value={firstname}
+                    onChange={(e) => setFirstname(e.target.value)}
+                  />
+                </div>
+              </div>
+              <div className="mb-4">
+                <label className="block text-gray-700">Last Name</label>
+                <div className="relative">
+                  <FaUser className="absolute left-3 top-4 text-gray-400" />
+                  <input
+                    type="text"
+                    className="w-full p-3 pl-10 border border-gray-300 rounded-lg text-black"
+                    value={lastname}
+                    onChange={(e) => setLastname(e.target.value)}
+                  />
+                </div>
+              </div>
+            </>
+          )}
           <div className="mb-4">
             <label className="block text-gray-700">Email</label>
             <div className="relative">
@@ -40,7 +95,7 @@ const LoginSignupModal = ({ isOpen, onClose }) => {
                 onChange={(e) => setPassword(e.target.value)}
               />
             </div>
-            {action === "Sign In" && (
+            {action === 'Sign In' && (
               <div className="text-right mt-2">
                 <a href="#" className="text-red-600 text-sm">Forgot password</a>
               </div>
@@ -62,17 +117,16 @@ const LoginSignupModal = ({ isOpen, onClose }) => {
           </button>
         </div>
         <p className="text-center text-gray-500">
-          {action === "Sign In" ? "Don't" : "Already"} have an account? 
+          {action === 'Sign In' ? "Don't" : 'Already'} have an account? 
           <button 
-            onClick={() => setAction(action === "Sign In" ? 'Sign Up' : 'Sign In')} 
+            onClick={() => setAction(action === 'Sign In' ? 'Sign Up' : 'Sign In')} 
             className="text-red-600 ml-1">
-            {action === "Sign In" ? "Sign Up" : "Sign In"}
+            {action === 'Sign In' ? 'Sign Up' : 'Sign In'}
           </button>
         </p>
-       
-        <button className="absolute top-5 right-5 text-white text-2xl" onClick={onClose}>
-            &times;
-          </button>
+        <button className="absolute top-5 right-5 text-black text-2xl" onClick={onClose}>
+          &times;
+        </button>
       </div>
     </div>
   );
