70cd39d263e8b7cf68b083cad4424312fa39e788
CyrilAshong <ashongcyril2704@gmail.com>
2024-07-22 11:41:17 +0000

    Added the userId as a response

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -15,9 +15,9 @@ router.post("/", async(req, res) => {
         const salt = await bcrypt.genSalt(Number(process.env.SALT));
         const hashPassword = await bcrypt.hash(req.body.password, salt);
 
-        await new User({...req.body, password: hashPassword}).save();
+        const newUser = await new User({...req.body, password: hashPassword}).save();
 
-        return res.status(201).send({message: "User Created Succecfully"})
+        return res.status(201).send({message: "User Created Succecfully", userId: newUser._id})
     } catch (error) {
        return res.status(500).send({message: "Internal Server Error"});
     }
