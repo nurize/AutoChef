718d3c1b76d8736f2b1e025262023622c4d7a848
CyrilAshong <ashongcyril2704@gmail.com>
2024-07-22 11:42:42 +0000

    Added an image route

--- Backend/index.js
+++ Backend/index.js
@@ -5,7 +5,11 @@ const cors = require("cors");
 const port = process.env.PORT || 8080;
 const connection = require("./db");
 const userRoutes = require("./routes/users");
-const authRoutes = require("./routes/auth")
+const authRoutes = require("./routes/auth");
+const bookRoutes = require("./routes/bookings")
+const serviceRoutes = require("./routes/services")
+const ImageRoute = require("./routes/images")
+
 
 
 // DB connection
@@ -19,6 +23,10 @@ app.use(cors());
 // Routes
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/booking", bookRoutes);
+app.use("/api/services", serviceRoutes);
+app.use("/api/images", ImageRoute);
+
 
 app.listen(port, () => {
     console.log(`Listening on Port ${port}....`);
