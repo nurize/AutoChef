7a06093bae83b2240255a988baa73ecb7d1cceaf
kwasi <affulnana04@gmail.com>
2024-08-09 23:14:56 +0000

    FR- Add ArrowUpButton to Statistic Card

---
+++ src/admin/components/StatisticCard.jsx
@@ -0,0 +1,29 @@
+import { Icon } from '@iconify/react';
+import React from 'react';
+import ArrowUpButton from './ArrowUpButton';  // Import ArrowUpButton
+
+const StatisticCard = ({ iconBgColor, iconColor, count, title, arrowUpBgColor, isBookedServices, bgColor, onArrowClick }) => (
+  <div className={`bg-white ${bgColor} p-4 border border-[#E8E9ED] rounded-lg group`}>
+    {!isBookedServices && (
+      <div className={`p-3 ${iconBgColor} w-fit mb-20 rounded-md group-hover:scale-110 transition-transform duration-300`}>
+        <Icon icon='gridicons:multiple-users' className={`h-6 w-6 ${iconColor}`} />
+      </div>
+    )}
+    <div className="flex justify-between">
+      <div>
+        <p className="text-lg md:text-xl font-semibold group-hover:text-white">{count}</p>
+        <h2 className="text-[#6E7379] group-hover:text-white">{title}</h2>
+      </div>
+      {!isBookedServices && (
+        <ArrowUpButton
+          onClick={() => onArrowClick(title)}
+          bgColor={arrowUpBgColor}  
+          borderStyle="border-none"
+          iconStyle="text-white h-6 w-6"  
+        />
+      )}
+    </div>
+  </div>
+);
+
+export default StatisticCard;
