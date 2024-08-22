3165ca38357de8938ca47526c6d2a77996e49527
kwasi <affulnana04@gmail.com>
2024-08-22 21:29:41 +0000

    change menu items colour

--- src/client/components/MenuItems.jsx
+++ src/client/components/MenuItems.jsx
@@ -16,7 +16,7 @@ const MenuItems = ({ isHeader = false, className = "" , setIsOpen }) => {
           <NavLink
             to={item.path}
             className={({ isActive }) => 
-              `hover:text-red-700 block w-fit ${isHeader ? 'py-2 md:py-3 lg:py-4 px-4 md:px-0' : '' } transition-colors duration-300 ${(isHeader && isActive) ? 'text-red-700 font-bold' : ''}`
+              `hover:text-[#DE0000] block w-fit ${isHeader ? 'py-2 md:py-3 lg:py-4 px-4 md:px-0' : '' } transition-colors duration-300 ${(isHeader && isActive) ? 'text-[#DE0000] font-bold' : ''}`
             }
             onClick={handleMenuItemClick}
           >
